17cf9e5cdd7c95a94a9bcbc630515ec7eadc531b
GeraDS10 <gerardosaldain10@gmail.com>
2023-10-10 01:40:46 -0300

    Se agrega movimiento a carrusel mobile y link a pagina juego

--- Interfaces/js/main.js
+++ Interfaces/js/main.js
@@ -1,4 +1,6 @@
 "use strict";
+
+
 let btn_registrar = document.querySelector("#btn_registrar");
 if(btn_registrar){
     btn_registrar.addEventListener("click",(event) => {
@@ -60,116 +62,274 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-const prevBtn = document.getElementById("btn_carrusel_izquierda");
-const nextBtn = document.getElementById("btn_carrusel_derecha");
-const carousel = document.querySelector(".carrusel");
 
-const cardsData = [
-    {
-        imgSrc: "../imagenes/cards/sugeridos/mario_bros.jpg",
-        nombre: "Mario Bros",
-        precio: "",
-        etiqueta: "$99",
-    },
-    {
-        imgSrc: "../imagenes/cards/sugeridos/mortal_kombat.jpg",
-        nombre: "Mortal Kombat",
-        precio: "",
-        etiqueta: "Agregado",
-    },
-    {
-        imgSrc: "../imagenes/cards/accion/bioshock.jpg",
-        nombre: "Bioshock",
-        precio: "",
-        etiqueta: "Gratis",
-    },
-    {
-        imgSrc: "../imagenes/cards/accion/dead_space.jpg",
-        nombre: "Dead Space",
-        precio: "",
-        etiqueta: "$89",
-    },
-    {
-        imgSrc: "../imagenes/cards/accion/farcry_4.jpg",
-        nombre: "Farcry 4",
-        precio: "",
-        etiqueta: "Agregado",
-    },
-    {
-        imgSrc: "../imagenes/cards/accion/freeguy.jpg",
-        nombre: "Free Guy",
-        precio: "",
-        etiqueta: "Gratis",
-    },
-];
 
-let currentIndex = 0;
+function carruselUnaCard() {
+    const prevBtn = document.getElementById("btn_carrusel_izquierda_mobile");
+    const nextBtn = document.getElementById("btn_carrusel_derecha_mobile");
+    const cardContainer = document.querySelector(".carrusel_mobile");
 
-function updateCarousel() {
-    const cardContainer = document.querySelector(".carrusel");
-    cardContainer.innerHTML = ""; 
+    const cardsData = [
+        {
+            imgSrc: "../imagenes/cards/accion/Hunt.jpg",
+            nombre: "Hunt",
+            precio: "",
+            etiqueta: "Gratis",
+        },
+        {
+            imgSrc: "../imagenes/marvel_vs_dc.png",
+            nombre: "Marvel vs DC",
+            precio: "",
+            etiqueta: "Gratis",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/halo_infinite.jpg",
+            nombre: "Halo Infinite",
+            precio: "",
+            etiqueta: "Agregado",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/warzone.jpg",
+            nombre: "Warzone",
+            precio: "",
+            etiqueta: "$49",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/Wolcen.jpg",
+            nombre: "Wolcen",
+            precio: "",
+            etiqueta: "Gratis",
+        },
+    ];
 
-    for (let i = currentIndex; i < currentIndex + 3; i++) {
-        const cardData = cardsData[i % cardsData.length];
-        if (cardData) {
-            const card = document.createElement("div");
-            card.classList.add("card");
+    let currentIndex = 0;
 
-            const img = document.createElement("img");
-            img.src = cardData.imgSrc;
-            img.alt = "";
-            img.classList.add("img_card");
+    function updateCarousel() {
+        const cardContainer = document.querySelector(".carrusel_mobile");
+        const cardData = cardsData[currentIndex];
 
-            const nombre = document.createElement("h3");
-            nombre.textContent = cardData.nombre;
-            nombre.classList.add("nombre_card");
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-            const precio = document.createElement("p");
-            precio.textContent = cardData.precio;
-            precio.classList.add("precio_card");
+        const img = document.createElement("img");
+        img.src = cardData.imgSrc;
+        img.alt = "";
+        img.classList.add("img_card");
 
-            const btnJugar = document.createElement("button");
-            if (cardData.etiqueta === "Gratis" ) {
-                btnJugar.textContent = "Jugar";
-            } else if (cardData.etiqueta.includes("$") || cardData.etiqueta === "Agregado") {
-                btnJugar.textContent = "Comprar";
-            }
-            btnJugar.classList.add("btn_card");
+        const nombre = document.createElement("h3");
+        nombre.textContent = cardData.nombre;
+        nombre.classList.add("nombre_card");
 
-            const containerEtiqueta = document.createElement("div");
-            const etiqueta = document.createElement("p");
-            etiqueta.textContent = cardData.etiqueta;
+        const precio = document.createElement("p");
+        precio.textContent = cardData.precio;
+        precio.classList.add("precio_card");
 
-            if (cardData.etiqueta === "Gratis") {
-                containerEtiqueta.classList.add("container_etiqueta_card_gratis");
-            } else if (cardData.etiqueta.startsWith("$")) {
-                containerEtiqueta.classList.add("container_etiqueta_card_comprar");
-            } else if (cardData.etiqueta === "Agregado") {
-                containerEtiqueta.classList.add("container_etiqueta_card_agregado");
-            }
-
-            etiqueta.classList.add("etiqueta_card");
-
-            card.appendChild(img);
-            card.appendChild(nombre);
-            card.appendChild(precio);
-            card.appendChild(btnJugar);
-            containerEtiqueta.appendChild(etiqueta);
-            card.appendChild(containerEtiqueta);
-
-            cardContainer.appendChild(card);
+        const btnJugar = document.createElement("button");
+        if (cardData.etiqueta === "Gratis") {
+            btnJugar.textContent = "Jugar";
+        } else if (cardData.etiqueta.includes("$") || cardData.etiqueta === "Agregado") {
+            btnJugar.textContent = "Comprar";
         }
+        btnJugar.classList.add("btn_card");
+
+        const containerEtiqueta = document.createElement("div");
+        const etiqueta = document.createElement("p");
+        etiqueta.textContent = cardData.etiqueta;
+
+        if (cardData.etiqueta === "Gratis") {
+            containerEtiqueta.classList.add("container_etiqueta_card_gratis");
+        } else if (cardData.etiqueta.startsWith("$")) {
+            containerEtiqueta.classList.add("container_etiqueta_card_comprar");
+        } else if (cardData.etiqueta === "Agregado") {
+            containerEtiqueta.classList.add("container_etiqueta_card_agregado");
+        }
+
+        etiqueta.classList.add("etiqueta_card");
+
+        card.appendChild(img);
+        card.appendChild(nombre);
+        card.appendChild(precio);
+        card.appendChild(btnJugar);
+        containerEtiqueta.appendChild(etiqueta);
+        card.appendChild(containerEtiqueta);
+
+        cardContainer.innerHTML = ""; 
+        cardContainer.appendChild(card);
+
+        if (cardData.nombre === "Marvel vs DC") {
+            btnJugar.setAttribute("id", "ir_juego");
+        }
+        let btn_ir_juego = document.querySelector("#ir_juego");
+            if(btn_ir_juego){
+                btn_ir_juego.addEventListener("click", () =>{                        
+                    let nuevaURL = "../html/juego.html"
+                    window.location.href = nuevaURL;
+                })
+            }
     }
+
+    nextBtn.addEventListener("click", () => {
+        if (currentIndex < cardsData.length - 1) {
+            currentIndex++;
+            updateCarousel();
+        }
+    });
+
+    prevBtn.addEventListener("click", () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateCarousel();
+        }
+    });
+
+    updateCarousel();
+    console.log("Carrusel de 1 card por carrusel");
 }
 
-nextBtn.addEventListener("click", () => {
-    currentIndex++;
-    updateCarousel();
-});
+function carruselTresCards() {
+    console.log("Carrusel de 3 cards por carrusel");
+    const prevBtn = document.getElementById("btn_carrusel_izquierda");
+    const nextBtn = document.getElementById("btn_carrusel_derecha");
 
-prevBtn.addEventListener("click", () => {
-    currentIndex--;
-    updateCarousel();
-});
+    const cardsData = [
+        {
+            imgSrc: "../imagenes/cards/sugeridos/mario_bros.jpg",
+            nombre: "Mario Bros",
+            precio: "",
+            etiqueta: "$99",
+        },
+        {
+            imgSrc: "../imagenes/cards/sugeridos/mortal_kombat.jpg",
+            nombre: "Mortal Kombat",
+            precio: "",
+            etiqueta: "Agregado",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/bioshock.jpg",
+            nombre: "Bioshock",
+            precio: "",
+            etiqueta: "Gratis",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/dead_space.jpg",
+            nombre: "Dead Space",
+            precio: "",
+            etiqueta: "$89",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/farcry_4.jpg",
+            nombre: "Farcry 4",
+            precio: "",
+            etiqueta: "Agregado",
+        },
+        {
+            imgSrc: "../imagenes/cards/accion/freeguy.jpg",
+            nombre: "Free Guy",
+            precio: "",
+            etiqueta: "Gratis",
+        },
+        {
+            imgSrc: "../imagenes/marvel_vs_dc.png",
+            nombre: "Marvel vs DC",
+            precio: "",
+            etiqueta: "Gratis",
+        },
+    ];
 
-updateCarousel();
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        const cardContainer = document.querySelector(".carrusel_desktop");
+        cardContainer.innerHTML = ""; 
+
+        for (let i = currentIndex; i < currentIndex + 3; i++) {
+            const cardData = cardsData[i % cardsData.length];
+            if (cardData) {
+                const card = document.createElement("div");
+                card.classList.add("card");
+
+                const img = document.createElement("img");
+                img.src = cardData.imgSrc;
+                img.alt = "";
+                img.classList.add("img_card");
+
+                const nombre = document.createElement("h3");
+                nombre.textContent = cardData.nombre;
+                nombre.classList.add("nombre_card");
+
+                const precio = document.createElement("p");
+                precio.textContent = cardData.precio;
+                precio.classList.add("precio_card");
+
+                const btnJugar = document.createElement("button");
+                
+                if (cardData.etiqueta === "Gratis" ) {
+                    btnJugar.textContent = "Jugar";
+                } else if (cardData.etiqueta.includes("$") || cardData.etiqueta === "Agregado") {
+                    btnJugar.textContent = "Comprar";
+                }
+                
+                btnJugar.classList.add("btn_card");
+
+                const containerEtiqueta = document.createElement("div");
+                const etiqueta = document.createElement("p");
+                etiqueta.textContent = cardData.etiqueta;
+
+                if (cardData.etiqueta === "Gratis") {
+                    containerEtiqueta.classList.add("container_etiqueta_card_gratis");
+                } else if (cardData.etiqueta.startsWith("$")) {
+                    containerEtiqueta.classList.add("container_etiqueta_card_comprar");
+                } else if (cardData.etiqueta === "Agregado") {
+                    containerEtiqueta.classList.add("container_etiqueta_card_agregado");
+                }
+
+                etiqueta.classList.add("etiqueta_card");
+
+                card.appendChild(img);
+                card.appendChild(nombre);
+                card.appendChild(precio);
+                card.appendChild(btnJugar);
+                containerEtiqueta.appendChild(etiqueta);
+                card.appendChild(containerEtiqueta);
+
+                cardContainer.appendChild(card);
+
+                if (cardData.nombre === "Marvel vs DC") {
+                    btnJugar.setAttribute("id", "ir_juego");
+                }
+                let btn_ir_juego = document.querySelector("#ir_juego");
+                    if(btn_ir_juego){
+                        btn_ir_juego.addEventListener("click", () =>{                        
+                            let nuevaURL = "../html/juego.html"
+                            window.location.href = nuevaURL;
+                        })
+                    }
+
+                
+            }
+        }
+    }
+
+    nextBtn.addEventListener("click", () => {
+        console.log("sarasa")
+        currentIndex++;
+        updateCarousel();
+    });
+
+    prevBtn.addEventListener("click", () => {
+        console.log("sarasas")
+        currentIndex--;
+        updateCarousel();
+    });
+
+    updateCarousel();
+
+}
+const anchoDispositivo = window.innerWidth;
+
+const umbralAncho = 600; 
+if (anchoDispositivo <= umbralAncho) {
+    carruselUnaCard();
+} else {
+    carruselTresCards();
+}

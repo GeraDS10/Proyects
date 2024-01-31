63f11fe14f1d7ec33ef09d2415ef71c24897120c
GeraDS10 <gerardosaldain10@gmail.com>
2024-01-31 16:17:16 -0300

    Update main.js

--- Guarderia_&_Peluqueria/js/main.js
+++ Guarderia_&_Peluqueria/js/main.js
@@ -39,7 +39,6 @@ let logo = document.querySelector("#logo").addEventListener("click",()=>{
 
 //------------------PARTIAL RENDER---------------------------------------------
 async function load_content(id) {
-    console.log("Loading content for {" + id + "}");
     let conteiner = document.querySelector("#content");
     try{
         let response = await fetch(`./${id}.html`);
@@ -47,11 +46,6 @@ async function load_content(id) {
             let text = await response.text();
             conteiner.innerHTML = text;
         }else{
-            console.log("implementando else");
-        //conteiner.innerHTML = 'error loading for /' + id + '...';
-        // conteiner.innerHTML = '<div class="logoCompletoDiv">' + 
-        //                         '<img src="../Guarderia_&_Peluqueria/images/LogoCompleto.png" alt="Logo" id="logoCompleto">' +
-        //                         '</div>';
         let id = "inicio";
         document.title = id;
         load_content(id);
@@ -73,7 +67,6 @@ async function load_content(id) {
 
 function push(event) {
     if(imgMenu.alt = "menuClose"){
-        console.log("Cerrando menu")
         deslizar();
     }
     let id = event.target.id;
@@ -98,7 +91,22 @@ window.addEventListener("popstate", event => {
 -------------PARTIAL RENDER
 */
 function estadia(){
-    const valorEstadia = 3500;
+    let valorEstadia;
+    const url = 'https://64e0b77950713530432c98b8.mockapi.io/api/peluqueria/Guarderia';
+    
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error de red - Código de estado: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        valorEstadia = data[0].valorEstadia;
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos:', error);
+      });
     let btnConsulta = document.querySelector("#buttonConsultar").addEventListener("click", calcular);
     let divResultado = document.querySelector("#resultadoDiv");
 
@@ -147,7 +155,6 @@ function estadia(){
     function calcularDiferenciaEnDias(fechaOrigen, fechaDestino) {
         const fecha1 = new Date(fechaOrigen);
         const fecha2 = new Date(fechaDestino);
-        console.log(fechaOrigen + " " + fechaDestino);
         const diferenciaEnMilisegundos = fecha2 - fecha1;
         const unDiaEnMilisegundos = 1000 * 60 * 60 * 24; // 1 día en milisegundos
         const diferenciaEnDias = Math.floor(diferenciaEnMilisegundos / unDiaEnMilisegundos);
@@ -162,142 +169,178 @@ function baño(){
     let divResultado = document.querySelector("#resultadoDiv");
 
     function calcular(){
-        let perroTamanio = document.querySelector("#perroTamanio");
-        let perroPelaje = document.querySelector("#perroPelaje");
-        let perroCorte = document.querySelector("#perroCorte");
-        let razaEjemplo;
-        const saltoLinea = "<br>";
-        const gigantePeloLargoCorte = 9200;
-        const gigantePeloLargoBanio = 8500;
-        const gigantePeloCortoBanio = 8000;
-        const grandePeloLargoCorte = 8200;
-        const grandePeloLargoBanio = 7000;
-        const grandePeloCortoBanio = 6000;
-        const medianoPeloLargoCorte = 6000;
-        const medianoPeloLargoBanio = 5000;
-        const medianoPeloCortoBanio = 4500;
-        const pequenioPeloLargoCorte = 4500;
-        const pequenioPeloLargoBanio = 3600;
-        const pequenioPeloCortoBanio = 3600;
+        let perroTamanio = document.querySelector("#perroTamanio").value;
+        let perroPelaje = document.querySelector("#perroPelaje").value;
+        let perroCorte = document.querySelector("#perroCorte").value;
+        let valorCorte, valorBanio, razaEjemplo, ejemplo;
+        if(perroPelaje == "largo"){
+            perroPelaje = true;
+
+        }
+        else{
+            perroPelaje = false;
+        }
+        
+        const url = 'https://64e0b77950713530432c98b8.mockapi.io/api/peluqueria/Peluqueria';
+        fetch(`${url}/?tamanio=${perroTamanio}&tienePelolargo=${perroPelaje}`)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error(`Error de red - Código de estado: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            valorCorte = data[0].corte;
+            valorBanio = data[0].banio;
+            ejemplo = data[0].ejemplo;
+            razaEjemplo = `<p> Raza Ejemplo : ${ejemplo} </p>`
+            const saltoLinea = "<br>";
+            if(perroCorte == "si"){
+                divResultado.innerHTML = "Valor estimado = " + valorCorte + saltoLinea + razaEjemplo;    
+            }else{
+                divResultado.innerHTML = "Valor estimado = " + valorBanio + saltoLinea + razaEjemplo;
+            }
+            
+
+        })
+        .catch(error => {
+            // Manejar errores de red o de la aplicación
+            console.error('Error al obtener los datos:', error);
+        });
+
+
+    //     const saltoLinea = "<br>";
+    //     const gigantePeloLargoCorte = 9200;
+    //     const gigantePeloLargoBanio = 8500;
+    //     const gigantePeloCortoBanio = 8000;
+    //     const grandePeloLargoCorte = 8200;
+    //     const grandePeloLargoBanio = 7000;
+    //     const grandePeloCortoBanio = 6000;
+    //     const medianoPeloLargoCorte = 6000;
+    //     const medianoPeloLargoBanio = 5000;
+    //     const medianoPeloCortoBanio = 4500;
+    //     const pequenioPeloLargoCorte = 4500;
+    //     const pequenioPeloLargoBanio = 3600;
+    //     const pequenioPeloCortoBanio = 3600;
         
        
-       switch(perroTamanio.value){
-        case 'gigante':
-            switch(perroPelaje.value){
-                case 'largo':
-                    switch(perroCorte.value){
-                        case 'si':
-                            razaEjemplo = "<p> Raza Ejemplo : Schnauzer Gigante </p>"
-                            divResultado.innerHTML = "Valor estimado = " + gigantePeloLargoCorte + saltoLinea + razaEjemplo;
-                        break;
+    //    switch(perroTamanio.value){
+    //     case 'gigante':
+    //         switch(perroPelaje.value){
+    //             case 'largo':
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         razaEjemplo = "<p> Raza Ejemplo : Schnauzer Gigante </p>"
+    //                         divResultado.innerHTML = "Valor estimado = " + gigantePeloLargoCorte + saltoLinea + razaEjemplo;
+    //                     break;
                         
-                        case 'no':
-                            razaEjemplo = "<p> Raza Ejemplo : Schnauzer Gigante </p>"
-                            divResultado.innerHTML = "Valor estimado = " + gigantePeloLargoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                    break;
-                case 'corto':
-                    razaEjemplo = "<p>Raza Ejemplo : Dogo Burdeos</p>";
-                    switch(perroCorte.value){
-                        case 'si':
-                            divResultado.innerHTML = "Valor estimado = " + gigantePeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         razaEjemplo = "<p> Raza Ejemplo : Schnauzer Gigante </p>"
+    //                         divResultado.innerHTML = "Valor estimado = " + gigantePeloLargoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //                 break;
+    //             case 'corto':
+    //                 razaEjemplo = "<p>Raza Ejemplo : Dogo Burdeos</p>";
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         divResultado.innerHTML = "Valor estimado = " + gigantePeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
 
-                        case 'no':
-                            divResultado.innerHTML = "Valor estimado = " + gigantePeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                break;      
-            }
-        break;
-        case 'grande':
-            switch(perroPelaje.value){
-                case 'largo':
-                    switch(perroCorte.value){
-                        case 'si':
-                            razaEjemplo = "<p> Raza Ejemplo : Golden Retriever </p>"
-                            divResultado.innerHTML = "Valor estimado = " + grandePeloLargoCorte + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         divResultado.innerHTML = "Valor estimado = " + gigantePeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //             break;      
+    //         }
+    //     break;
+    //     case 'grande':
+    //         switch(perroPelaje.value){
+    //             case 'largo':
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         razaEjemplo = "<p> Raza Ejemplo : Golden Retriever </p>"
+    //                         divResultado.innerHTML = "Valor estimado = " + grandePeloLargoCorte + saltoLinea + razaEjemplo;
+    //                     break;
                         
-                        case 'no':
-                            razaEjemplo = "<p> Raza Ejemplo : Golden Retriever </p>"
-                            divResultado.innerHTML = "Valor estimado = " + grandePeloLargoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                    break;
-                case 'corto':
-                    razaEjemplo = "<p>Raza Ejemplo : Labrador</p>";
-                    switch(perroCorte.value){
-                        case 'si':
-                            divResultado.innerHTML = "Valor estimado = " + grandePeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         razaEjemplo = "<p> Raza Ejemplo : Golden Retriever </p>"
+    //                         divResultado.innerHTML = "Valor estimado = " + grandePeloLargoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //                 break;
+    //             case 'corto':
+    //                 razaEjemplo = "<p>Raza Ejemplo : Labrador</p>";
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         divResultado.innerHTML = "Valor estimado = " + grandePeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
 
-                        case 'no':
-                            divResultado.innerHTML = "Valor estimado = " + grandePeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                break;      
-            }
-        break;
-        case 'mediano':
-            switch(perroPelaje.value){
-                case 'largo':
-                    switch(perroCorte.value){
-                        case 'si':
-                            razaEjemplo = "<p>Raza Ejemplo : Border Collie</p>";
-                            divResultado.innerHTML = "Valor estimado = " + medianoPeloLargoCorte + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         divResultado.innerHTML = "Valor estimado = " + grandePeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //             break;      
+    //         }
+    //     break;
+    //     case 'mediano':
+    //         switch(perroPelaje.value){
+    //             case 'largo':
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         razaEjemplo = "<p>Raza Ejemplo : Border Collie</p>";
+    //                         divResultado.innerHTML = "Valor estimado = " + medianoPeloLargoCorte + saltoLinea + razaEjemplo;
+    //                     break;
 
-                        case 'no':
-                            razaEjemplo = "<p>Raza Ejemplo : Border Collie</p>";
-                            divResultado.innerHTML = "Valor estimado = " + medianoPeloLargoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                    break;
-                case 'corto':
-                    razaEjemplo = "<p>Raza Ejemplo : Boxer</p>";
-                    switch(perroCorte.value){
-                        case 'si':
-                            divResultado.innerHTML = "Valor estimado = " + medianoPeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         razaEjemplo = "<p>Raza Ejemplo : Border Collie</p>";
+    //                         divResultado.innerHTML = "Valor estimado = " + medianoPeloLargoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //                 break;
+    //             case 'corto':
+    //                 razaEjemplo = "<p>Raza Ejemplo : Boxer</p>";
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         divResultado.innerHTML = "Valor estimado = " + medianoPeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
 
-                        case 'no':
-                            divResultado.innerHTML = "Valor estimado = " + medianoPeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                break;      
-            }
-        break;
-        case 'chico':
-            switch(perroPelaje.value){
-                case 'largo':
-                    razaEjemplo = "<p>Raza Ejemplo : Caniche Toy</p>";
-                    switch(perroCorte.value){
-                        case 'si':
-                            divResultado.innerHTML = "Valor estimado = " + pequenioPeloLargoCorte + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         divResultado.innerHTML = "Valor estimado = " + medianoPeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //             break;      
+    //         }
+    //     break;
+    //     case 'chico':
+    //         switch(perroPelaje.value){
+    //             case 'largo':
+    //                 razaEjemplo = "<p>Raza Ejemplo : Caniche Toy</p>";
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         divResultado.innerHTML = "Valor estimado = " + pequenioPeloLargoCorte + saltoLinea + razaEjemplo;
+    //                     break;
 
-                        case 'no':
-                            divResultado.innerHTML = "Valor estimado = " + pequenioPeloLargoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                    break;
-                case 'corto':
-                    razaEjemplo = "<p>Raza Ejemplo : Jack Russell</p>";
-                    switch(perroCorte.value){
-                        case 'si':
-                            divResultado.innerHTML = "Valor estimado = " + pequenioPeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
+    //                     case 'no':
+    //                         divResultado.innerHTML = "Valor estimado = " + pequenioPeloLargoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //                 break;
+    //             case 'corto':
+    //                 razaEjemplo = "<p>Raza Ejemplo : Jack Russell</p>";
+    //                 switch(perroCorte.value){
+    //                     case 'si':
+    //                         divResultado.innerHTML = "Valor estimado = " + pequenioPeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
 
-                        case 'no':
-                            divResultado.innerHTML = "Valor estimado = " + pequenioPeloCortoBanio + saltoLinea + razaEjemplo;
-                        break;
-                    }
-                break;      
-            }
-        break;
-       }
+    //                     case 'no':
+    //                         divResultado.innerHTML = "Valor estimado = " + pequenioPeloCortoBanio + saltoLinea + razaEjemplo;
+    //                     break;
+    //                 }
+    //             break;      
+    //         }
+    //     break;
+    //    }
        window.scrollBy(0, divResultado.clientHeight * 5);
     }
 }
